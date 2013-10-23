b0117bd2a6e513a6957f0419000e9c472de1e332
fabiosn1 <fabiosn1__@hotmail.com>
2013-10-23 17:38:29 -0200

    Segundo commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 function body_OnLoad(){
 	$("#datepicker").datepicker();
+	$(".ui-datepicker").css({"width": "70em", "height": "30em", "position": "relative", "left": "5%"});
+//	$(".ui-datepicker-calendar td").css({"width": "30em", "height": "5em", "background-color": "yellow"});
+	$(".ui-datepicker-calendar td a").css({"background-color": "gray",  "border-style": "none", "width": "10em", "height": "5em", "font-color": "white"})
 }
