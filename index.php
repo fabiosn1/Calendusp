8f55f21aecfe11ecfd3fc875767aa43f1c074730
fabiosn1 <fabiosn1__@hotmail.com>
2013-12-05 10:32:45 -0200

    Quinto commit

--- index.php
+++ index.php
@@ -3,109 +3,42 @@
 
 <html>
     <head>  
-    
-        <!-- include jquery mobile -->
+	 <!-- include jquery mobile -->
         <link rel="stylesheet" href="css/jquery.mobile-1.3.2.min.css" />
-	    <script src="js/jquery-1.9.1.min.js"></script>
-	    <script src="js/jquery.mobile-1.3.2.min.js"></script>     
+	<script src="js/jquery-1.9.1.min.js"></script>
+	<script src="js/jquery.mobile-1.3.2.min.js"></script>     
        
     	<!-- include jquery -->
     	<link rel="stylesheet" href="css/jquery.mobile.structure-1.3.2.min.css" />
     	<script src="js/jquery-1.9.1.min.js"></script>
     	<script src="js/jquery.mobile-1.3.2.min.js"></script>
     	
- 		<!-- include jquery ui -->
+ 	<!-- include jquery ui -->
     	<link rel="stylesheet" href="css/jquery-ui.css" />
-  		<script src="js/jquery-1.9.1.min.js"></script>
-  		<script src="js/jquery-ui.js"></script>
-  		
-  		<script src="js/app.js"> </script>
+	<script src="js/jquery-1.9.1.min.js"></script>
+  	<script src="js/jquery-ui.js"></script>
+	
+		
+       
+	<!-- include calendar -->    
+	<link href='css/fullcalendar.css' rel='stylesheet' />
+	<link href='css/fullcalendar.print.css' rel='stylesheet' media='print' />
+	<script src="js/fullcalendar.js"> </script>
+	<script src="js/fullcalendar.min.js"> </script>
+
+	<script src="js/app.js"> </script>
+	
 	</head>  		
-	<body>
-		<?php 
-
-		    $info = array();
-		    $monthNumbers = array();
-		    $day = array();
-		    $name = array();	
-		    $month = array();			
-						    
-		    # array containing months of the year
-		    $months = array(
-				"blank",
-				"janeiro",
-				"fevereiro",
-				"março",
-				"abril",
-				"maio",
-				"junho",
-				"julho",
-				"agosto",
-				"setembro",
-				"outubro",
-				"novembro",
-				"dezembro",
-			);
-		    
-		    # creates and loads simple html dom parser
-
-		    include('simple_html_dom.php');
-			
-			
-			$html = file_get_html('https://uspdigital.usp.br/jupiterweb/jupCalendario2014.jsp');
-			
-			$tdelements = $html->find('td'); 
-
-			$i = 4;			
-			$j = 0;
-			$k = 0;
-			while($i < 39){
-				if(($monthNumber = isMonth($tdelements[$i]->plaintext)) != 0){
-					$info[$j]= $i;
-					$monthNumbers[$j] = $monthNumber;
-					$j++;
-				}
-
-				$i++;				
-			}			
-			
-			$info[$j] = $info[$j-1]+4;
-				
-			for($i = 0; $i < count($info); $i++){
-				$j = $info[$i]+1;
-				while($j < $info[$i+1]){
-					$day[$k] = $tdelements[$j]->plaintext.'.'.$monthNumbers[$i];
-					$name[$k] = $tdelements[++$j]->plaintext;						 
-					$j++;
-					$k++;
-				}
-				
-			
-			}
-
-			$output = array_combine($day, $name);
-                        
-			echo 'callback('.json_encode($output).')';
-
-			function isMonth($value){
-					
-				# puts $value to lower case and checks whether it's a month
-				global $months;
-				
-		                $value = strtolower($value);
-				for($j = 0; $j < count($months) && strlen($value) != 0; $j++){
-					if(strpos($value, $months[$j]) !== FALSE){
-						return $j;
-					}
-				}
-				return 0;
-			} 
-			
-		?>
-		<div id="headerDiv" data-role="header">
-    	  <h2> Calendusp </h2>
+	<body onload="body_OnLoad()">
+		
+	<div id="headerDiv" data-role="header">
+    		<h2> Calendusp </h2>
     	</div>
-    	<div id="datepicker">
-    	</div>    	
+	<div id="calendar">	
+		
+	</div>
+
+    <!--	<div id="datepicker">
+    	</div>    	-->
 	</body>
 </html>
--- js/app.js
+++ js/app.js
@@ -1,27 +1,51 @@
 function body_OnLoad(){
-	$("#datepicker").datepicker();
-	$(".ui-datepicker").css({"width": $(window).width(), "height": $(window).height()*0.8, "position": "relative"});
+//	$("#datepicker").datepicker();
+//	$(".ui-datepicker").css({"width": $(window).width(), "height": $(window).height()*0.8, "position": "relative"});
 //	$(".ui-datepicker-calendar td").css({"width": "30em", "height": "5em", "background-color": "yellow"});
-	$(".ui-datepicker-calendar td a").css({"background-color": "#FDB523",  "border-style": "none", "width": "10em", "height": "5em", "font-color": "white"})
+//	$(".ui-datepicker-calendar td a").css({"background-color": "#FDB523",  "border-style": "none", "width": "10em", "height": "5em", "font-color": "white"})
 	//.ui-datepicker-title
-	$(".ui-datepicker-title").css({"background-color": "#64A8D3"})
+//	$(".ui-datepicker-title").css({"background-color": "#64A8D3"})
 	//.ui-datepicker-title select
-	$(".ui-datepicker-header").css({"background-color": "#64A8D3"})
-	getData();
+//	$(".ui-datepicker-header").css({"background-color": "#64A8D3"})
+	var url = "http://rad2013.brunoramos.eti.br/index.php";
+	var script = document.createElement("script");
+	script.setAttribute("src", url);
+	document.head.appendChild(script);
+
 }
 
-function getData(){
-	    console.log('getData');
-        var url = "zon.allalla.com";
-        var script = document.createElement("script");
-        script.setAttribute("src", url);
-        document.head.appendChild(script);
-        console.log('endGetData');
-}
 
 function callback(data){
-	var holidays = JSON.parse(data);
-	console.log('callback');
-	console.log(data);
+		console.log('callback: ' + data);	
+		$(document).ready(function() {
 	
-}
+		$('#calendar').fullCalendar({
+		
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			
+			editable: true,
+			
+			events: data,
+			
+			
+			eventDrop: function(event, delta) {
+				alert(event.title + ' was moved ' + delta + ' days\n' +
+					'(should probably update your database)');
+			},
+			
+			loading: function(bool) {
+				if (bool) $('#loading').show();
+				else $('#loading').hide();
+			}
+			
+		});
+		
+	});	
+
+
+}	
+
